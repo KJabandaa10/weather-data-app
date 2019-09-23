ee1d8887c90c7c30432c23a6b7a76d4a94526301
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-09-23 14:36:43 +0100

    add clear searches button

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const button = document.querySelector("button");
+const button = document.querySelector(".button");
 const input = document.getElementById("input");
+const clearButton = document.querySelector(".clear");
+
 async function getWeather() {
   let input = document.querySelector("input").value;
   let response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=1d4cd589716fb70b2c7958b584c58a01&units=metric`
   );
   const weatherData = await response.json();
-  console.log(weatherData);
   // access main json object
   const main = weatherData.main;
-  console.log(main);
-  console.log(weatherData.name);
   // get main weather info
   let mainArr = [main.humidity, main.temp, main.temp_max, main.temp_min];
   // store weather request in LocalStorage
@@ -18,30 +17,39 @@ async function getWeather() {
 
   // display main info on page
   const cityName = document.createElement("p");
-  cityName.textContent = input;
-  document.body.appendChild(cityName);
+  cityName.setAttribute("id", "result");
+  cityName.textContent = `The weather for ${input}`;
+  document.getElementById("data-div").appendChild(cityName);
   cityName.style.fontSize = "24px";
   const humidity = document.createElement("p");
+  humidity.setAttribute("id", "result");
   humidity.textContent = `Humidity: ${mainArr[0]}`;
-  document.body.appendChild(humidity);
+  document.getElementById("data-div").appendChild(humidity);
   const temp = document.createElement("p");
+  temp.setAttribute("id", "result");
   temp.textContent = `Temperature: ${mainArr[1]}°`;
-  document.body.appendChild(temp);
+  document.getElementById("data-div").appendChild(temp);
   const maxTemp = document.createElement("p");
+  maxTemp.setAttribute("id", "result");
   maxTemp.textContent = `Maximum temperature: ${mainArr[2]}°`;
-  document.body.appendChild(maxTemp);
+  document.getElementById("data-div").appendChild(maxTemp);
   const minTemp = document.createElement("p");
+  minTemp.setAttribute("id", "result");
   minTemp.textContent = `Minimum temperature: ${mainArr[3]}°`;
-  document.body.appendChild(minTemp);
+  document.getElementById("data-div").appendChild(minTemp);
+}
+
+function clearSearches() {
+  let removeArr = [];
+  document.querySelectorAll("#result").forEach(result => {
+    result.remove();
+  });
 }
 
 button.addEventListener("click", getWeather);
-
 input.onkeydown = function(e) {
   if (e.keyCode == 13) {
     getWeather();
   }
 };
-
-// for tests
-getWeather();
+clearButton.addEventListener("click", clearSearches);
